dc4f665450c32fa146c210dc2c12f8f80b9fc82e
Pi-jey <p.mixer2002@gmail.com>
2021-02-13 13:36:08 +0200

    [upd] добавил пару файлов

--- function.py
+++ function.py
@@ -1,8 +1,40 @@
+# import modules
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0.01, 2, 0.01)
-y = np.log10(x)/np.arctan(x)
+# our function
+def func(x) -> float:
+    return np.tan(x)/x
 
-plt.plot(x, y)
-plt.show()
+def build_plot():
+    # input begin and end of our plot
+    begin = - np.pi
+    end =  np.pi
+    # input step
+    step = 0.1
+
+
+    # calculate drawing quality
+    drawing_quality = int((end - begin) / step)
+
+    # create a drawing with a coordinate plane
+    plt.subplots()
+    # create an area in which there will be - the graph is displayed
+    # x values to be displayed
+    # number of elements in the created array
+    # graph drawing quality
+    x = np.linspace(begin, end, drawing_quality)
+    # draw a graph
+    plt.plot(x, func(x))
+    # showing the schedule
+    plt.show()
+
+    # output our table of results
+    while begin < end:
+        print('x =', begin, '\t| y =', func(begin))
+        begin += step
+
+if __name__ == '__main__':
+    build_plot()
+
+
